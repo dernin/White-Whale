3aa9e593b756bf4712f23746b4a1b92164c34b92
Devin <dc97080@gmail.com>
2021-12-11 15:37:57 -0600

    response 299

--- pages/api/[query].js
+++ pages/api/[query].js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
             populateCollection('caching', {name: author, search: phrase})
         }
         
-        return res.status(102).json({error: 'caching'})
+        return res.status(299).json({error: 'caching'})
     } else {
         return res.status(200).json(results[0])
     }
