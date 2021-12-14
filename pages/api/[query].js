229082124eda45734d85940fbbce179b9ee86cf0
Devin <dc97080@gmail.com>
2021-12-14 16:47:45 -0600

    injection protection

--- pages/api/[query].js
+++ pages/api/[query].js
@@ -9,23 +9,32 @@ export default async function handler(req, res) {
     phrase = phrase.toLowerCase()
     //const test = urlParams.get('test')
 
-    
+    const paramsSearch = {
+        name: `${author}_search_${phrase}`,
+    }
 
     const results = await runQuery(
-        `FOR file IN searches FILTER file.name == '${author}_search_${phrase}' RETURN file`
+        `FOR file IN searches FILTER file.name == @name RETURN file`,
+        paramsSearch
     )
 
     // if no results, send back false
     if (results.length == 0) {
-
         // to keep it from running searchPhrase more than once
-        const checkCache = await runQuery(`FOR file IN caching FILTER file.name == '${author}' AND file.search == '${phrase}' RETURN file `)
+        const paramsCache = {
+            author: author,
+            term: phrase,
+        }
+        const checkCache = await runQuery(
+            `FOR file IN caching FILTER file.name == @author AND file.search == @term RETURN file `,
+            paramsCache
+        )
         if (checkCache.length == 0) {
             searchPhrase(phrase, author)
-            populateCollection('caching', {name: author, search: phrase})
+            populateCollection('caching', { name: author, search: phrase })
         }
-        
-        return res.status(299).json({error: 'caching'})
+
+        return res.status(299).json({ error: 'caching' })
     } else {
         return res.status(200).json(results[0])
     }
